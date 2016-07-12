44efc250786136fc0b697b91c193919eb244c379
millylee <millylee@qq.com>
2016-07-13 02:05:19 +0800

    update theme

--- themes/topic/scripts/helpers.js
+++ themes/topic/scripts/helpers.js
@@ -6,18 +6,20 @@ var url = require('url');
 var cheerio = require('cheerio');
 var lunr = require('lunr');
 
-hexo.extend.helper.register('top_menu', function(){
-  var menu = this.theme.menu;
+hexo.extend.helper.register('top_menu', function(category){
+  var menu = this.theme.menu[category];
   var pathname = '/' + pathFn.basename(this.path);
   var result = '';
   var self = this;
 
   _.each(menu, function(path, title){
-      if (pathname === path) {
-        result += '<li class="active" role="presentation"><a href="' + self.url_for(path) + '">' + self.__('menu.' + title) + '</a></li>';
-      } else {
-        result += '<li role="presentation"><a href="' + self.url_for(path) + '">' + self.__('menu.' + title) + '</a></li>';
-      }      
+    //path -> guide/css.html
+    //pathname -> /css.html
+    if (path.indexOf(pathname) !== -1) {
+      result += '<li class="active" role="presentation"><a href="' + self.url_for(path) + '">' + self.__('menu.' + category +'.' + title) + '</a></li>';
+    } else {
+      result += '<li role="presentation"><a href="' + self.url_for(path) + '">' + self.__('menu.' + category +'.' + title) + '</a></li>';
+    }      
   });
 
   return result;
